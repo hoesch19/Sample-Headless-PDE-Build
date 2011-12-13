ab79eed42278fc9b90311b6144442fe04ed6bcb3
Erwin Hoesch <erwin.hoesch@googlemail.com>
2011-12-13 21:56:55 +0100

    Anpassungen an das Laptop

--- plugins/de.hoesch.erwin.eclipse.headless.build/build.cmd
+++ plugins/de.hoesch.erwin.eclipse.headless.build/build.cmd
@@ -1,8 +1,9 @@
 
 echo Ist Baustelle gesetzt?
-set ECLIPSE_HOME=%BAUSTELLE%\eclipse-SDK-3.7.1-Xtext-2.1.0-win32-x86_64
+set ECLIPSE_HOME=%BAUSTELLE%\eclipse
+set PROJECT=headless
 
 
 
-%ECLIPSE_HOME%\eclipsec.exe -consolelog -application org.eclipse.pde.build.Build -DbuildId=2011-%1 -Dbaustelle=%BAUSTELLE%
+%ECLIPSE_HOME%\eclipsec.exe -consolelog -application org.eclipse.pde.build.Build -DbuildId=2011-%1 -Dbaustelle=%BAUSTELLE% -Dproject=%PROJECT%
 
